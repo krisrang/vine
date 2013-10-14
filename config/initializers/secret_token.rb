57355d5e4316d3991bfd66bf4f550023dad9b162
Kristjan Rang <mail@rang.ee>
2013-10-14 12:30:05 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vine::Application.config.secret_key_base = '4ce083b599a251170009e8e1c6e77005a6b7c7b5a7364ede865cbe95aee287ad2f20f0bf1d7ca04aec04e1d4300af9aaea0bab4e156bb30379b3fe0699262d18'
