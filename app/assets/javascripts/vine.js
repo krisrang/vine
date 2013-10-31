ad3f8662acbc3acd136def38e468b3674ef4c31b
Kristjan Rang <mail@rang.ee>
2013-10-31 13:20:30 +0200

    Finish MessageBus setup

--- app/assets/javascripts/vine.js
+++ app/assets/javascripts/vine.js
@@ -1,8 +1,42 @@
 Vine = Ember.Application.createWithMixins(Vine.Ajax, {
+  hasFocus: true,
+
   getURL: function(url) {
     // If it's a non relative URL, return it.
     if (url.indexOf('http') === 0) return url;
     return url;
+  },
+
+  titleChanged: function() {
+    var title = "";
+    if (this.get('title')) {
+      title += "" + (this.get('title')) + " - ";
+    }
+    title += Vine.SiteSettings.title;
+    $('title').text(title);
+
+    var notifyCount = this.get('notifyCount');
+    if (notifyCount > 0 && !Vine.User.currentProp('dynamic_favicon')) {
+      title = "(" + notifyCount + ") " + title;
+    }
+
+    // chrome bug workaround see: http://stackoverflow.com/questions/2952384/changing-the-window-title-when-focussing-the-window-doesnt-work-in-chrome
+    window.setTimeout(function() {
+      document.title = ".";
+      document.title = title;
+    }, 200);
+  }.observes('title', 'hasFocus', 'notifyCount'),
+
+  faviconChanged: function() {
+    if(Vine.User.currentProp('dynamic_favicon')) {
+      new Favcount(Vine.SiteSettings.favicon_url).set(
+        this.get('notifyCount')
+      );
+    }
+  }.observes('notifyCount'),
+
+  notifyTitle: function(count) {
+    this.set('notifyCount', count);
   },
 
   logout: function() {
@@ -28,6 +62,13 @@ Vine = Ember.Application.createWithMixins(Vine.Ajax, {
   },
 
   initDom: function() {
+    $(window).focus(function() {
+      Vine.set('hasFocus', true);
+      Vine.set('notify', false);
+    }).blur(function() {
+      Vine.set('hasFocus', false);
+    });
+
     Vine.csrfToken = $('meta[name=csrf-token]').attr('content');
 
     $.ajaxPrefilter(function(options, originalOptions, xhr) {
@@ -42,9 +83,25 @@ Vine = Ember.Application.createWithMixins(Vine.Ajax, {
     });
   },
 
+  subscribeToNotifications: function() {
+    var user = Vine.User.current();
+
+    if (user) {
+      var bus = Vine.MessageBus;
+      bus.callbackInterval = Vine.SiteSettings.polling_interval;
+      bus.enableLongPolling = true;
+
+      bus.subscribe("/refresh-browser", function(data){
+        return document.location.reload(true);
+      });
+    }
+  },
+
   start: function() {
     Vine.initDom();
     Vine.MessageBus.alwaysLongPoll = Vine.Environment === "development";
     Vine.MessageBus.start();
+
+    Vine.subscribeToNotifications();
   }
 });
