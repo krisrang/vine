71457ee51cd741a7947f20137eabcaddd6ce1899
Kristjan Rang <mail@rang.ee>
2013-11-29 17:02:08 +0200

    Working on user profiles messages

--- app/assets/javascripts/app/controllers/messages_controller.js
+++ app/assets/javascripts/app/controllers/messages_controller.js
@@ -1,22 +1,4 @@
 Vine.MessagesController = Vine.ArrayController.extend({
   sortProperties: ['createdAt'],
-  sortAscending: false,
-
-  actions: {
-  },
-
-  filteredContent: (function() {
-    var result, sortedResult;
-
-    result = this.filter(function(item, index) {
-      return !(item.get('isNew'));
-    });
-
-    sortedResult = Em.ArrayProxy.createWithMixins(
-      Ember.SortableMixin, 
-      { content:result, sortProperties: this.sortProperties, sortAscending: this.sortAscending }
-    );
-
-    return sortedResult;
-  }).property('content.isLoaded', 'content.@each.isNew')
+  sortAscending: false
 });
